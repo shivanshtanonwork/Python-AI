0fbd01bb2569a6c088fa62dffc50bdd441a44624
shivanshtanonwork <shivanshtandonwork@gmail.com>
2026-06-04 12:01:21 +0530

    Thread using functions

--- 10_thread_concurrency/thread_basics.py
+++ 10_thread_concurrency/thread_basics.py
@@ -1,22 +1,28 @@
 from threading import Thread
 from time import sleep
 
-class Hello(Thread):
-    def run(self):
-        for i in range(5):
-            print("Hello Threads", i+1)
-            sleep(1)
+def hello():
+    for i in range(5):
+        print("Hello Threads", i+1)
+        sleep(1)
 
-class Hi(Thread):
-    def run(self):
-        for i in range(5):
-            print("Hi Threads", i+1)
-            sleep(1)
+
+def hi():
+    for i in range(5):
+        print("Hi Threads", i+1)
+        sleep(1)
+   
             
 if __name__ == "__main__":
-    obj1 = Hello()
-    obj2 = Hi()
+    obj1 = Thread(target=hello)
+    sleep(0.5)
+    obj2 = Thread(target=hi)
 
     obj1.start()
     obj2.start()
 
+
+    obj1.join()
+    obj2.join()
+
+    print("bye")
